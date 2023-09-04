48d637d08ed36749551017b2fe38e2fd64d44fd7
Mehmet Talha Aksoy <talhaaksoyy@gmail.com>
2023-09-04 16:56:45 +0300

    a

---
+++ Blog/Models/ViewModel/MakaleVM.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Models.Entities;
+
+namespace Blog.Models.ViewModel
+{
+    public class MakaleVM
+    {
+        public ICollection<Makale> Makaleler { get; set; }
+        public Makale Makale { get; set; }
+        public User Yazar { get; set; }
+        public ICollection<User> Users { get; set; }
+        public ICollection<Konu> Konular { get; set; }
+    }
+}
