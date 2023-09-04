4db21fc9f288cc425185fd5ecf1e1fab2b16c7bb
Sabri ŞEN <sabrisen97@gmail.com>
2023-09-04 15:52:14 +0300

    tertemiz push

--- Blog/Models/Entities/User.cs
+++ Blog/Models/Entities/User.cs
@@ -8,13 +8,8 @@ namespace Blog.Models.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
-<<<<<<< HEAD
         public string? Aciklama { get; set; }
         public string? Image { get; set; }
-=======
-        public string Aciklama { get; set; }
-        public string Image { get; set; }
->>>>>>> d7d51f3a9139dc4b5d0e06acc339d406c540fb95
         public ICollection<Makale>? Makales { get; set; }
         public ICollection<UserMakale>? UserMakales { get; set; }
     }
